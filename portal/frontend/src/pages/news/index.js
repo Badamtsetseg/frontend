d27b989d9bdc32a1d6b4acf299c73333d815a6ff
Tsogtbayar Bayankhishig <tbayankhis@gmail.com>
2021-04-21 16:53:42 +0800

    Update index.js

--- portal/frontend/src/pages/news/index.js
+++ portal/frontend/src/pages/news/index.js
@@ -1,54 +1,19 @@
+import {useEffect} from 'react'
 import {NextSeo} from 'next-seo'
 import Link from 'next/link'
+import {observer, inject} from 'mobx-react'
 import {Breadcrumb, Row, Col} from 'antd'
 import {HomeOutlined} from '@ant-design/icons'
 
+import {Loader} from '../../components/elements'
+
 import styles from './news.module.less'
 
-const NewsList = ({children}) => {
+const NewsBase = observer(({children, referenceDataStore, referenceDataStore: {selectData, selectLoading}}) => {
 
-  const menu = [
-    {
-      title: 'Сүүлд нэмэгдсэн',
-      id: 'latest'
-    },
-    {
-      title: 'Онцлох',
-      id: 'important'
-    },
-    {
-      title: 'Цаг үе',
-      id: 'time'
-    },
-    {
-      title: 'Фото мэдээ',
-      id: 'photo'
-    },
-    {
-      title: 'Видео мэдээ',
-      id: 'video'
-    },
-    {
-      title: 'Атар-60',
-      id: 'atar-60'
-    },
-    {
-      title: 'Хөнгөн-85',
-      id: 'khungun-85'
-    },
-    {
-      title: 'Хэвлэлийн мэдээ',
-      id: 'article'
-    },
-    {
-      title: 'Урилга',
-      id: 'invitation'
-    },
-    {
-      title: 'Их уншсан',
-      id: 'most'
-    }
-  ]
+  useEffect(() => {
+    referenceDataStore.fetchSelect({typeCode: 'COVID_TYPE'})
+  }, [])
   
   return (
     <>
@@ -59,39 +24,41 @@ const NewsList = ({children}) => {
             <Breadcrumb.Item href=''>
               <HomeOutlined />
             </Breadcrumb.Item>
-            <Breadcrumb.Item href=''>
-              <span>Мэдээ, мэдээлэл</span>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>Application</Breadcrumb.Item>
+            <Breadcrumb.Item>Мэдээ, мэдээлэл</Breadcrumb.Item>
           </Breadcrumb>
         </div>
       </div>
       <br />
       <div className='container'>
-        <Row gutter={15}>
-          <Col xs={24} sm={24} md={12} lg={5}>
-            <div className={styles.menu}>
-              <div className={styles.title}>
-                <span>Мэдээ, мэдээлэл</span>
+        {selectLoading ? 
+          <Loader />
+          :
+          <Row gutter={15}>
+            <Col xs={24} sm={24} md={12} lg={5}>
+              <div className={styles.menu}>
+                <div className={styles.title}>
+                  <span>Мэдээ, мэдээлэл</span>
+                </div>
+                <ul>
+                  {selectData.map(item => (
+                    <li key={item.id}>
+                      <Link as={'/news/latest'} href='/news/[category]'>
+                        <a>{item.name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </div>
-              <ul>
-                {menu.map(item => (
-                  <li key={item.id}>
-                    <Link as={`/news/${item.id}`} href='/news/[category]'>
-                      <a>{item.title}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Col>
-          <Col xs={0} sm={24} md={12} lg={19}>
-            {children}
-          </Col>
-        </Row>
+            </Col>
+            <Col xs={0} sm={24} md={12} lg={19}>
+              {children}
+            </Col>
+          </Row>
+        }
+        
       </div>
     </>
   )
-}
+})
 
-export default NewsList
+export default inject('referenceDataStore')(NewsBase)
